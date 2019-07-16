8a603afca4a76ec371859f8b9cc8fad831bcd4e0
Jannetta Steyn <jannetta@jannetta.com>
2019-07-16 13:38:09 +0100

    Change KeyEvent library import

--- src/main/java/uk/ac/ncl/cemdit/view/utilities/OrderFile.java
+++ src/main/java/uk/ac/ncl/cemdit/view/utilities/OrderFile.java
@@ -1,13 +1,10 @@
 package uk.ac.ncl.cemdit.view.utilities;
 
-import com.sun.glass.events.KeyEvent;
-
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
-import java.io.*;
+import java.awt.event.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.Scanner;
 import java.util.Vector;
 
