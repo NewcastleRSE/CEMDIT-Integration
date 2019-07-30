3e23150605e69de916b4ac5d23d411355e7baa2b
Jannetta S Steyn <jannetta@jannetta.com>
2019-07-30 18:25:28 +0100

    Why did this not push before?

--- src/main/java/uk/ac/ncl/cemdit/controller/integration/Utils.java
+++ src/main/java/uk/ac/ncl/cemdit/controller/integration/Utils.java
@@ -6,6 +6,7 @@ import uk.ac.ncl.cemdit.model.integration.IntegrationModel;
 import uk.ac.ncl.cemdit.model.integration.QueryResults;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 public class Utils {
 
@@ -22,11 +23,14 @@ public class Utils {
         integrationModel.setTopRankedQuery("sensor(theme(Vehicles),Sensor_name, sensor_centroid_latitude, sensor_centroid_longitude, timestamp, units, count)");
 
         ArrayList<Object> row = new ArrayList<>();
+        Date date = new Date ();
+        long t = 1546301013;
+        date.setTime((long)t*1000);
         row.add(0, "Vehicle");
         row.add(1, "VEHICLE_COUNT_01");
         row.add(2, "43.8499984741");
         row.add(3, "-1.6118754505");
-        row.add(4, "1546301013000");
+        row.add(4, date);
         row.add(5, "Vehicles");
         row.add(6, "11");
         data1.add(0, row);
@@ -36,19 +40,18 @@ public class Utils {
         row1.add(1, "VEHICLE_COUNT_01");
         row1.add(2, "43.8488884741");
         row1.add(3, "-1.6118754505");
-        row1.add(4, "1546301013000");
+        row1.add(4, date);
         row1.add(5, "Vehicles");
         row1.add(6, "6");
         data1.add(1, row1);
 
+        String[] columnNames = {"Theme", "Sensor Name", "Sensor centroid latitud","Sensor centroid longitude","Timestamp","Units","Count"};
+        Class[] columnClasses = {String.class, String.class, Float.class, Float.class, Date.class,String.class, Integer.class};
+        integrationDataModel.setColumnNames(columnNames);
         integrationDataModel.setData(data1);
-        integrationDataModel.setColumName(0, "Theme");
-        integrationDataModel.setColumName(1, "Sensor Name");
-        integrationDataModel.setColumName(2, "Sensor centroid latitude");
-        integrationDataModel.setColumName(3, "Sensor centroid longitude");
-        integrationDataModel.setColumName(4, "Timestamp");
-        integrationDataModel.setColumName(5, "Units");
-        integrationDataModel.setColumName(6, "Count");
+        integrationDataModel.setClasses(columnClasses);
+
+
         System.out.println("Set data.");
 
 
@@ -65,6 +68,6 @@ public class Utils {
         }
         integrationModel.setQueryResults(queryResults);
         integrationModel.setProvNFilename("/home/campus.ncl.ac.uk/njss3/Dropbox/CEM-DIT/CHAIn/Mockups/count_prov.svg");
-        integrationDataModel.fire();
+//        integrationDataModel.fire();
     }
 }
