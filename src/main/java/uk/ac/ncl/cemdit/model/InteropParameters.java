523c6c156e44c91cdbdd022f53d105552cf37dde
jsteyn <jannetta.steyn@newcastle.ac.uk>
2019-12-02 14:23:09 +0000

    Class to hold parameters for call to openprovenance conversion tool

---
+++ src/main/java/uk/ac/ncl/cemdit/model/InteropParameters.java
@@ -0,0 +1,238 @@
+package uk.ac.ncl.cemdit.model;
+
+public class InteropParameters{
+    private String verbose = null;
+    private String debug = null;
+    private String logfile = null;
+    private String infile = null;
+    private String informat = null;
+    private String outfile = null;
+    private String outformat = null;
+    private String namespaces = null;
+    private String title = null;
+    private String layout = null;
+    private String bindings = null;
+    private String bindingformat = null;
+    private String generator = null;
+    private String index = null;
+    private String flatten = null;
+    private String merge = null;
+    private String compare = null;
+    private String compareOut = null;
+    private String template = null;
+    private String bpackage = null;
+    private String location = null;
+    private int bindingsVersion = 3;
+    private boolean addOrderp = false;
+    private boolean listFormatsp = false;
+    private boolean allexpanded = false;
+    private String lastdir = "";
+
+    public  String getVerbose() {
+        return verbose;
+    }
+
+    public  void setVerbose(String verbose) {
+        verbose = verbose;
+    }
+
+    public  String getDebug() {
+        return debug;
+    }
+
+    public  void setDebug(String debug) {
+        debug = debug;
+    }
+
+    public  String getLogfile() {
+        return logfile;
+    }
+
+    public  void setLogfile(String logfile) {
+        logfile = logfile;
+    }
+
+    public  String getInfile() {
+        return infile;
+    }
+
+    public  void setInfile(String infile) {
+        infile = infile;
+    }
+
+    public  String getInformat() {
+        return informat;
+    }
+
+    public  void setInformat(String informat) {
+        informat = informat;
+    }
+
+    public  String getOutfile() {
+        return outfile;
+    }
+
+    public  void setOutfile(String outfile) {
+        outfile = outfile;
+    }
+
+    public  String getOutformat() {
+        return outformat;
+    }
+
+    public  void setOutformat(String outformat) {
+        outformat = outformat;
+    }
+
+    public  String getNamespaces() {
+        return namespaces;
+    }
+
+    public  void setNamespaces(String namespaces) {
+        namespaces = namespaces;
+    }
+
+    public  String getTitle() {
+        return title;
+    }
+
+    public  void setTitle(String title) {
+        title = title;
+    }
+
+    public  String getLayout() {
+        return layout;
+    }
+
+    public  void setLayout(String layout) {
+        layout = layout;
+    }
+
+    public  String getBindings() {
+        return bindings;
+    }
+
+    public  void setBindings(String bindings) {
+        bindings = bindings;
+    }
+
+    public  String getBindingformat() {
+        return bindingformat;
+    }
+
+    public  void setBindingformat(String bindingformat) {
+        bindingformat = bindingformat;
+    }
+
+    public  String getGenerator() {
+        return generator;
+    }
+
+    public  void setGenerator(String generator) {
+        generator = generator;
+    }
+
+    public  String getIndex() {
+        return index;
+    }
+
+    public  void setIndex(String index) {
+        index = index;
+    }
+
+    public  String getFlatten() {
+        return flatten;
+    }
+
+    public  void setFlatten(String flatten) {
+        flatten = flatten;
+    }
+
+    public  String getMerge() {
+        return merge;
+    }
+
+    public  void setMerge(String merge) {
+        merge = merge;
+    }
+
+    public  String getCompare() {
+        return compare;
+    }
+
+    public  void setCompare(String compare) {
+        compare = compare;
+    }
+
+    public  String getCompareOut() {
+        return compareOut;
+    }
+
+    public  void setCompareOut(String compareOut) {
+        compareOut = compareOut;
+    }
+
+    public  String getTemplate() {
+        return template;
+    }
+
+    public  void setTemplate(String template) {
+        template = template;
+    }
+
+    public  String getBpackage() {
+        return bpackage;
+    }
+
+    public  void setBpackage(String bpackage) {
+        bpackage = bpackage;
+    }
+
+    public  String getLocation() {
+        return location;
+    }
+
+    public  void setLocation(String location) {
+        location = location;
+    }
+
+    public  int getBindingsVersion() {
+        return bindingsVersion;
+    }
+
+    public  void setBindingsVersion(int bindingsVersion) {
+        bindingsVersion = bindingsVersion;
+    }
+
+    public  boolean isAddOrderp() {
+        return addOrderp;
+    }
+
+    public  void setAddOrderp(boolean addOrderp) {
+        addOrderp = addOrderp;
+    }
+
+    public  boolean isListFormatsp() {
+        return listFormatsp;
+    }
+
+    public  void setListFormatsp(boolean listFormatsp) {
+        listFormatsp = listFormatsp;
+    }
+
+    public  boolean isAllexpanded() {
+        return allexpanded;
+    }
+
+    public  void setAllexpanded(boolean allexpanded) {
+        allexpanded = allexpanded;
+    }
+
+    public  String getLastdir() {
+        return lastdir;
+    }
+
+    public  void setLastdir(String lastdir) {
+        lastdir = lastdir;
+    }
+}
