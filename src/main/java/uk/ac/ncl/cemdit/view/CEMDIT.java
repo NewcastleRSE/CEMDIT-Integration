0d46b62c7388d4198b2534d957fe66c2a5dfa4f4
jsteyn <jannetta.steyn@newcastle.ac.uk>
2019-12-16 16:20:17 +0000

    Change GUI to use JTabbedPane

--- src/main/java/uk/ac/ncl/cemdit/view/CEMDIT.java
+++ src/main/java/uk/ac/ncl/cemdit/view/CEMDIT.java
@@ -1,11 +1,12 @@
 package uk.ac.ncl.cemdit.view;
 //http://localhost:8086/query/?theme=Vehicles&type=Vehicle count&sensor=PER_TRF_CNT_SL_A690D1&starttime=20190101000000&endtime=2019013123595959
+
 import org.apache.log4j.Logger;
 import uk.ac.ncl.cemdit.controller.ComponentPointers;
+import uk.ac.ncl.cemdit.controller.integration.Utils;
 import uk.ac.ncl.cemdit.model.integration.IntegrationDataModel;
 import uk.ac.ncl.cemdit.model.integration.IntegrationModel;
-import uk.ac.ncl.cemdit.view.integration.CEMDITMainPanel;
-import uk.ac.ncl.cemdit.view.integration.QueryPanel;
+import uk.ac.ncl.cemdit.view.integration.*;
 
 import javax.swing.*;
 import javax.swing.event.ListSelectionEvent;
@@ -13,15 +14,16 @@ import javax.swing.event.ListSelectionListener;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 
-public class CEMDIT extends JFrame  implements ActionListener, ListSelectionListener {
+public class CEMDIT extends JFrame implements ActionListener, ListSelectionListener {
     private Logger logger = Logger.getLogger(this.getClass());
     ComponentPointers cp = ComponentPointers.getInstance();
 
     /**
      * The main panel containing all the other panels
      */
-    private CEMDITMainPanel mainPanel ;
+    private CEMDITMainPanel mainPanel;
 
     /**
      * A tabbed pane for all the subpanels
@@ -29,9 +31,20 @@ public class CEMDIT extends JFrame  implements ActionListener, ListSelectionList
     private JTabbedPane tabbedpane = new JTabbedPane();
 
     /**
+     * Panel containing the top ranked response (or else the selected response)
+     */
+    private ResultsPanel resultPanel;
+    private QueryPanel queryPanel;
+    /**
+     * Panel containing all the returned results
+     */
+    private JScrollPane sp_dataPanel;
+    private DataPanel dataPanel = new DataPanel();
+
+    /**
      * Model for data table
      */
-    static private IntegrationDataModel integrationDataModel = new IntegrationDataModel( );
+    static private IntegrationDataModel integrationDataModel = new IntegrationDataModel();
 
     /**
      * Class containing pointers to everything (all components of GUI etc)
@@ -39,7 +52,7 @@ public class CEMDIT extends JFrame  implements ActionListener, ListSelectionList
     static private IntegrationModel integrationModel = new IntegrationModel();
 
 
-    CEMDIT() {
+    private CEMDIT() {
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         String logo = "CEMDITLogo3.png";
         Toolkit toolkit = Toolkit.getDefaultToolkit();
@@ -53,8 +66,15 @@ public class CEMDIT extends JFrame  implements ActionListener, ListSelectionList
 
 //        Utils.populateIntegrationModel("sensor(theme(Vehicles),Sensor_name, sensor_centroid_latitude, sensor_centroid_longitude, timestamp, units, count)",integrationModel,integrationDataModel);
         mainPanel = new CEMDITMainPanel();
-        QueryPanel queryPanel = new QueryPanel(integrationModel, integrationDataModel, this);
-        tabbedpane.add(queryPanel, "Query");
+        resultPanel = new ResultsPanel(integrationModel, integrationDataModel, this);
+        queryPanel = new QueryPanel(integrationModel, integrationDataModel, this);
+        dataPanel.setDataModel(integrationDataModel);
+        sp_dataPanel = new JScrollPane(dataPanel);
+        sp_dataPanel.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+        sp_dataPanel.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        sp_dataPanel.setPreferredSize(new Dimension(900, 250));
+        tabbedpane.add("Query", queryPanel);
+        tabbedpane.add("Query Results", sp_dataPanel);
 //        getContentPane().add(mainPanel);
         getContentPane().add(tabbedpane);
         pack();
@@ -69,7 +89,41 @@ public class CEMDIT extends JFrame  implements ActionListener, ListSelectionList
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        logger.debug("Action: " + e.getActionCommand());
+        switch (e.getActionCommand()) {
+            case "Run":
+                if (queryPanel.getProvQueryType().getSelectedIndex() > 0) {
+                    QueryType qtype = QueryType.SQL;
+                    if (queryPanel.getRdf().isSelected()) qtype = QueryType.RDF;
+                    if (queryPanel.getRest().isSelected()) qtype = QueryType.REST;
+                    if (queryPanel.getSql().isSelected()) qtype = QueryType.SQL;
+                    logger.debug("Populate model.");
+                    String query = queryPanel.getQueryTextArea().getText();
+                    if (query == null || query.equals("")) {
+                        JOptionPane.showMessageDialog(this, "Select the type of query before running the query.");
 
+                    } else {
+                        // Save the query to the properties file to use as default for next startup
+                        ComponentPointers.setProperty("query", query);
+                        File dir = new File(System.getProperty("user.home"));
+                        // populate the Integration model with the results from the query
+                        Utils.populateIntegrationModel(query, integrationModel, integrationDataModel, qtype, this);
+                        resultPanel.getDataPanel().setDataModel(integrationDataModel);
+                        //queryTextArea.setText(integrationModel.getOriginalQuery());
+                        resultPanel.getRepairedQuery().setText(integrationModel.getTopRankedQuery());
+                        resultPanel.getMatchPanel().populateMatchPanel(integrationModel.getQueryResults());
+                        if (resultPanel.getDataPanel().getRowCount() > 0) {
+                            resultPanel.getBtn_saveData().setEnabled(true);
+                            resultPanel.getBtn_saveJSON().setEnabled(true);
+                        } else {
+                            resultPanel.getBtn_saveData().setEnabled(false);
+                            resultPanel.getBtn_saveJSON().setEnabled(false);
+                        }
+                        integrationModel.getProvenancePanel().setLoaded(false);
+                    }
+                    break;
+                }
+        }
     }
 
     @Override
