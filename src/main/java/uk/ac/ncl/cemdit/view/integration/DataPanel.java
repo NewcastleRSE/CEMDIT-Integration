79af7d6fc01ef2fcc3a8100e31dac9114a3b421a
Jannetta S Steyn <jannetta@jannetta.com>
2019-12-17 06:29:27 +0000

    Add comments

--- src/main/java/uk/ac/ncl/cemdit/view/integration/DataPanel.java
+++ src/main/java/uk/ac/ncl/cemdit/view/integration/DataPanel.java
@@ -5,12 +5,19 @@ import uk.ac.ncl.cemdit.model.integration.IntegrationDataModel;
 import javax.swing.*;
 import javax.swing.table.TableModel;
 
+/**
+ * A table for holding and displaying the results returned by a query
+ */
 public class DataPanel extends JTable {
 
     public DataPanel() {
         super();
     }
 
+    /**
+     * Set the datamodel for the table
+     * @param integrationDataModel
+     */
     public void setDataModel(TableModel integrationDataModel) {
         this.setModel(integrationDataModel);
     }
