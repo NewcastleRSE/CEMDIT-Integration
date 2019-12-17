0a0219c9115624d6f806e8c8c275c6776635fa2f
Jannetta S Steyn <jannetta@jannetta.com>
2019-12-17 23:40:01 +0000

    Some comment cleanup

--- src/main/java/uk/ac/ncl/cemdit/view/CEMDIT.java
+++ src/main/java/uk/ac/ncl/cemdit/view/CEMDIT.java
@@ -5,6 +5,7 @@ import org.apache.log4j.Logger;
 import uk.ac.ncl.cemdit.controller.ComponentPointers;
 import uk.ac.ncl.cemdit.controller.integration.LookupType;
 import uk.ac.ncl.cemdit.controller.integration.Utils;
+import uk.ac.ncl.cemdit.model.InteropParameters;
 import uk.ac.ncl.cemdit.model.integration.IntegrationDataModel;
 import uk.ac.ncl.cemdit.model.integration.IntegrationModel;
 import uk.ac.ncl.cemdit.view.integration.*;
@@ -15,11 +16,12 @@ import javax.swing.event.ListSelectionListener;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.net.URL;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.Scanner;
+import java.util.Set;
 
 public class CEMDIT extends JFrame implements ActionListener, ListSelectionListener {
     private Logger logger = Logger.getLogger(this.getClass());
@@ -54,7 +56,7 @@ public class CEMDIT extends JFrame implements ActionListener, ListSelectionListe
     /**
      * Panel containing all the matches
      */
-    MatchPanel matchPanel = new MatchPanel();
+    MatchPanelNew matchPanel = new MatchPanelNew();
 
     /**
      * Model for data table
@@ -124,17 +126,7 @@ public class CEMDIT extends JFrame implements ActionListener, ListSelectionListe
                         File dir = new File(System.getProperty("user.home"));
                         // populate the Integration model with the results from the query
                         Utils.populateIntegrationModel(query, integrationModel, integrationDataModel, qtype, this);
-                        resultPanel.getDataTable().setDataModel(integrationDataModel);
-                        //queryTextArea.setText(integrationModel.getOriginalQuery());
-                        resultPanel.getRepairedQuery().setText(integrationModel.getTopRankedQuery());
-                        resultPanel.getMatchPanel().populateMatchPanel(integrationModel.getQueryResults());
-                        if (resultPanel.getDataTable().getRowCount() > 0) {
-                            resultPanel.getBtn_saveData().setEnabled(true);
-                            resultPanel.getBtn_saveJSON().setEnabled(true);
-                        } else {
-                            resultPanel.getBtn_saveData().setEnabled(false);
-                            resultPanel.getBtn_saveJSON().setEnabled(false);
-                        }
+                        matchPanel.populateMatchPanel(integrationModel.getQueryResults());
                         integrationModel.getProvenancePanel().setLoaded(false);
                         tabbedpane.setSelectedIndex(1);
                     }
@@ -142,9 +134,109 @@ public class CEMDIT extends JFrame implements ActionListener, ListSelectionListe
                     JOptionPane.showMessageDialog(this, "Select the provenance template before running the query.");
                 }
                 break;
-            case "comboBoxChanged":
+            case "Bind Data to Template":
+                final JFileChooser fc2 = new JFileChooser(ComponentPointers.getLastDir());
+                fc2.setCurrentDirectory(new java.io.File(ComponentPointers.getLastDir()));
+                int returnVal2 = fc2.showOpenDialog(this);
+                if (returnVal2 == JFileChooser.APPROVE_OPTION) {
+                    File file = fc2.getSelectedFile();
+                    int writeFile = 0;
+                    if (file.exists()) {
+                        writeFile = JOptionPane.showConfirmDialog(
+                                this,
+                                file.getName() + " exists. Do you want to overwrite it?",
+                                "File exists.",
+                                JOptionPane.YES_NO_OPTION);
+                    }
+                    if (writeFile == 0) {
+                        // Lookup template
+                        // Check in properties file where lookup database is stored
+                        ComponentPointers componentPointers = ComponentPointers.getInstance();
+                        String lookup = ComponentPointers.getProperty("lookupdb");
+                        // What lookup type (i.e. sensor) has been selected from the combo box?
+                        Utils.lookupProvenance(Enum.valueOf(LookupType.class, lookup), integrationModel, queryPanel.getProvQueryType().getSelectedItem().toString());
+                        integrationModel.getProvenancePanel().loadGraph(false);
+                        // Get provstore location
+                        try {
+                            String indexFileName = file.getAbsolutePath() + "_MergeIndex.txt";
+                            PrintWriter indexfile = new PrintWriter(indexFileName);
+                            InputStream in = new URL(integrationModel.getProvNFilename()).openStream();
+                            componentPointers.setProvnfile(integrationModel.getProvNFilename());
+                            logger.trace("provn filename: " + integrationModel.getProvNFilename());
+                            // Read the provn from the provStore
+                            String loadPROVN = Utils.orderFile(new Scanner(in, "UTF-8").useDelimiter("\\A").next());
 
-                logger.debug("Selected item: " + queryPanel.getProvQueryType().getSelectedItem());
+                            String filename = file.getAbsolutePath();
+                            try {
+                                InteropParameters interopParameters = new InteropParameters();
+                                String directory = file.getParent();
+                                // Save data
+                                int rows = integrationDataModel.getRowCount();
+                                for (int row = 0; row < rows; row++) {
+                                    File bindingdatafile = new File(file.getAbsolutePath() + "_" + row + ".json");
+                                    PrintWriter pw = new PrintWriter(bindingdatafile);
+                                    pw.println("{\"var\": {");
+                                    LinkedHashMap<String, Object> hashMap = integrationDataModel.getRowAsHashMap(row);
+                                    //Populate dynamically from HashMap
+                                    //Give elements names starting with a capital and attributes a name start with lower case
+                                    // Use this feature to distinguish between id and value
+                                    Set keys = hashMap.keySet();
+                                    for (Iterator iterator = keys.iterator(); iterator.hasNext(); ) {
+                                        String K = (String) iterator.next();
+                                        Object V = hashMap.get(K);
+                                        pw.format("\"%s\": [{\"" + ((int) K.toString().charAt(0) < 90
+                                                ? "@id\":\"uo:%s\""
+                                                : "@value\":\"%s\", \"@type\": \"xsd:string\"")
+                                                + "}]" + (iterator.hasNext() ? "," : "") + "\n", K, V);
+                                    }
+                                    pw.format("},\n");
+                                    pw.println("\"context\":{\"ex\": \"http://example.org/\",\"uo\": \"http://urbanobservatory.ac.uk/\"}");
+                                    pw.println("\n}\"");
+                                    pw.close();
+                                }
+                                for (int row = 0; row < rows; row++) {
+                                    File bindingdatafile = new File(file.getAbsolutePath() + "_" + row + ".json");
+                                    File outputbounddata = new File(file.getAbsolutePath() + "_bound_" + row + ".provn");
+                                    // Bind data to template
+                                    interopParameters.setInfile(ComponentPointers.getProperty("tempdir") + "\\temporary.provn");
+                                    interopParameters.setInformat("provn");
+                                    interopParameters.setOutfile(outputbounddata.getAbsolutePath());
+                                    interopParameters.setOutformat("provn");
+                                    interopParameters.setBindings(bindingdatafile.getAbsolutePath());
+                                    interopParameters.setBindingformat("");
+                                    interopParameters.setBindingsVersion(3);
+                                    Utils.bindDataToTemplate(interopParameters);
+                                    if (row == rows - 1) indexfile.format("file, %s, provn\n", outputbounddata);
+                                    else indexfile.format("file, %s, provn,\n", outputbounddata);
+                                }
+                                indexfile.close();
+                                // Merge files
+                                interopParameters.resetValues();
+                                interopParameters.setMerge(indexFileName);
+                                interopParameters.setOutfile(file.getAbsolutePath() + "_mergedFile.provn");
+                                logger.trace(interopParameters.toString());
+                                Utils.bindDataToTemplate(interopParameters);
+                                interopParameters.setFlatten("flatten");
+                                interopParameters.setOutfile(file.getAbsolutePath() + "_MergedFile_Flattened.provn");
+                                logger.trace(interopParameters.toString());
+                                Utils.bindDataToTemplate(interopParameters);
+
+                                // Run ProvenanceExplorer
+                                ProvenanceExplorer provenanceExplorer = new ProvenanceExplorer("Provenance Explorer");
+                                provenanceExplorer.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+                                // load provn into text pane
+                                componentPointers.getTextPanes().getProvN().setText(loadPROVN);
+                                // load graphs into graphic panes
+                                componentPointers.loadProvnFile(new File(ComponentPointers.getProperty("tempdir") + "/temporary.provn"));
+                            } catch (FileNotFoundException e1) {
+                                logger.trace("File not found: " + e1.getMessage());
+                            }
+                        } catch (IOException e1) {
+                            logger.trace("IOException: " + e1.getMessage());
+                        }
+                    }
+                }
+
                 break;
             case "View Prov Template":
                 if (queryPanel.getProvQueryType().getSelectedIndex() > 0) {
