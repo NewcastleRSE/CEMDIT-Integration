6bf6f64fe884bfd901cd335a91a1ac8577813fab
Jannetta S Steyn <jannetta@jannetta.com>
2019-12-17 23:39:23 +0000

    An initial commit of the model for the MatchTable

---
+++ src/main/java/uk/ac/ncl/cemdit/model/integration/MatchTableModel.java
@@ -0,0 +1,46 @@
+package uk.ac.ncl.cemdit.model.integration;
+
+import javax.swing.table.AbstractTableModel;
+import java.util.ArrayList;
+
+public class MatchTableModel extends AbstractTableModel {
+    String[] columnNames = {"Source",
+            "Operator",
+            "Target",
+            "Match"};
+
+    ArrayList<QueryResults> data = new ArrayList<>();
+
+    public void setData(ArrayList<QueryResults> data) {
+        this.data = data;
+    }
+
+    @Override
+    public int getRowCount() {
+        return data.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return columnNames.length;
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        switch (columnIndex) {
+            case 0:
+                return data.get(rowIndex).getLabel();
+            case 1:
+                return data.get(rowIndex).getOperator();
+            case 2:
+                return data.get(rowIndex).getValue();
+        }
+        return null;
+    }
+
+    @Override
+    public String getColumnName(int col) {
+        return columnNames[col];
+    }
+
+}
