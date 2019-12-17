88390cc16100f4c0f65d4c21559d1c9ebfe3aecb
Jannetta S Steyn <jannetta@jannetta.com>
2019-12-17 23:38:26 +0000

    Initial commit of a new MatchPanel for the tabbed version of the GUI

---
+++ src/main/java/uk/ac/ncl/cemdit/view/integration/MatchPanelNew.java
@@ -0,0 +1,45 @@
+package uk.ac.ncl.cemdit.view.integration;
+
+import uk.ac.ncl.cemdit.model.integration.MatchTableModel;
+import uk.ac.ncl.cemdit.model.integration.QueryResults;
+
+import javax.swing.*;
+import java.util.ArrayList;
+
+public class MatchPanelNew extends JPanel {
+
+    JLabel similarityScore = new JLabel("Similarity Score: " );
+
+    /**
+     * A table for displaying the matches returned from CHAIn
+     */
+    MatchTable matchTable = new MatchTable();
+    JScrollPane sp_matchTable = new JScrollPane(matchTable);
+
+    /**
+     * Constructor
+     */
+    public MatchPanelNew() {
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+        add(similarityScore);
+        add(sp_matchTable);
+    }
+
+    /**
+     * @param queryResults
+     */
+    public void populateMatchPanel(ArrayList<QueryResults> queryResults) {
+        MatchTableModel matchTableModel = new MatchTableModel();
+        matchTableModel.setData(queryResults);
+        matchTable.setDataModel(matchTableModel);
+        matchTable.updateUI();
+    }
+
+    public JLabel getSimilarityScore() {
+        return similarityScore;
+    }
+
+    public void setSimilarityScore(JLabel similarityScore) {
+        this.similarityScore = similarityScore;
+    }
+}
