dcbc69180ec5cb12740c9a90ffe926ab9f2fa5b6
Jannetta S Steyn <jannetta@jannetta.com>
2019-12-21 21:04:02 +0000

    Add re-submit button for query after operators have been changed from combobox

--- src/main/java/uk/ac/ncl/cemdit/view/integration/MatchPanelNew.java
+++ src/main/java/uk/ac/ncl/cemdit/view/integration/MatchPanelNew.java
@@ -5,37 +5,44 @@ import uk.ac.ncl.cemdit.model.integration.MatchTableModel;
 import uk.ac.ncl.cemdit.model.integration.QueryResults;
 
 import javax.swing.*;
+import javax.swing.table.TableColumn;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
 public class MatchPanelNew extends JPanel {
     private Logger logger = Logger.getLogger(this.getClass());
-
-    JLabel similarityScore = new JLabel("Similarity Score: " );
+    private JButton reSubmit = new JButton("Re-submit");
+    private JLabel similarityScore = new JLabel("Similarity Score: ");
 
     /**
      * A table for displaying the matches returned from CHAIn
      */
-    MatchTable matchTable = new MatchTable();
-    JScrollPane sp_matchTable = new JScrollPane(matchTable);
+    private MatchTable matchTable = new MatchTable();
+    private JScrollPane sp_matchTable = new JScrollPane(matchTable);
 
     /**
      * Constructor
      */
-    public MatchPanelNew() {
+    public MatchPanelNew(ActionListener actionListener) {
         setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
         add(similarityScore);
+        reSubmit.addActionListener(actionListener);
+        add(reSubmit);
         add(sp_matchTable);
     }
 
     /**
-     * @param queryResults
+     * @param queryResults An ArrayList of type QueryResults
      */
     public void populateMatchPanel(ArrayList<QueryResults> queryResults) {
+        String[] relations = {" ", "=", "<", ">", "!"};
+        JComboBox comboBox = new JComboBox(relations);
         MatchTableModel matchTableModel = new MatchTableModel();
         matchTableModel.setData(queryResults);
         matchTable.setDataModel(matchTableModel);
-        matchTableModel.fireTableStructureChanged();
-        matchTable.updateUI();
+        TableColumn matchColumn = matchTable.getColumnModel().getColumn(3);
+        matchColumn.setCellEditor(new MatchTableCBCellEditor(relations));
+        matchColumn.setCellRenderer(new MatchTableCBCellRenderer(relations));
     }
 
     public JLabel getSimilarityScore() {
