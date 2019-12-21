36c36c927b5155c87b0887099c8d929917ac34a5
Jannetta S Steyn <jannetta@jannetta.com>
2019-12-21 21:25:18 +0000

    Override methods required for combobox to work

--- src/main/java/uk/ac/ncl/cemdit/model/integration/MatchTableModel.java
+++ src/main/java/uk/ac/ncl/cemdit/model/integration/MatchTableModel.java
@@ -1,15 +1,19 @@
 package uk.ac.ncl.cemdit.model.integration;
 
+import org.apache.log4j.Logger;
+
+import javax.swing.*;
 import javax.swing.table.AbstractTableModel;
 import java.util.ArrayList;
 
 public class MatchTableModel extends AbstractTableModel {
-    String[] columnNames = {"Source",
+    private Logger logger = Logger.getLogger(this.getClass());
+    private String[] columnNames = {"Source",
             "Operator",
             "Target",
             "Match"};
 
-    ArrayList<QueryResults> data = new ArrayList<>();
+    private ArrayList<QueryResults> data = new ArrayList<>();
 
     public MatchTableModel() {
         super();
@@ -38,6 +42,8 @@ public class MatchTableModel extends AbstractTableModel {
                 return data.get(rowIndex).getOperator();
             case 2:
                 return data.get(rowIndex).getValue();
+            case 3:
+                return data.get(rowIndex).getNewoperator();
 
         }
         return null;
@@ -48,4 +54,26 @@ public class MatchTableModel extends AbstractTableModel {
         return columnNames[col];
     }
 
+    @Override
+    public boolean isCellEditable(int row, int col) {
+        //Note that the data/cell address is constant,
+        //no matter where the cell appears onscreen.
+        if (col < 3) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    @Override
+    public Class getColumnClass(int c) {
+        return getValueAt(0, c).getClass();
+    }
+
+    @Override
+    public void setValueAt(Object value, int row, int col) {
+        QueryResults q = data.get(row);
+        q.setNewoperator((String)value.toString().substring(0,1));
+        fireTableCellUpdated(row, col);
+    }
 }
